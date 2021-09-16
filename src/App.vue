29097afeaa573f2b766f7bc84e1fa493418314b0
fkkmemi <fkkmemi@gmail.com>
2021-09-16 22:22:47 +0900

    3 개발환경 구축하기 quasar

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <router-view />
+  <div>ijii</div>
+  <h>jiji</h>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App'
+})
+</script>
