3fe8f5b8785d802a58d441e4b87bdce9763b384d
fkkmemi <fkkmemi@gmail.com>
2021-09-28 09:12:33 +0900

    7 quasar 스타일 알아보기

--- src/pages/TestVue.vue
+++ src/pages/TestVue.vue
@@ -1,62 +1,27 @@
 <template>
-  <q-page padding>
-    <q-card>
-      <q-card-section>
-        text: {{ addedText }}
-      </q-card-section>
-      <q-card-section>
-        <q-input v-model="text" />
-      </q-card-section>
-      <q-card-actions>
-        <q-btn
-          flat
-          label="add"
-          @click="add"
-        />
-      </q-card-actions>
-      <q-card-section>
-        compo...
-        <TestCompo :text="text" />
-      </q-card-section>
-    </q-card>
-  </q-page>
+  <div
+    class="text-h1 text-weight-thin q-ma-sm q-pa-md"
+    style="background-color: red"
+  >
+    new
+    <q-btn
+      color="deep-purple-9"
+      icon="check"
+      label="OK"
+    />
+  </div>
+  <div class="shadow-3 q-ma-sm fixed rotate-45">
+    iii
+  </div>
 </template>
 <script lang="ts">
-import { defineComponent, ref, computed, provide } from 'vue'
-import TestCompo from 'components/TestCompo.vue'
+import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'TestVue',
-  components: { TestCompo },
-  // data () {
-  //   return {
-  //     text: 'abc'
-  //   }
-  // },
-  // computed: {
-  //   addedText (): string {
-  //     return this.text + ' added'
-  //   }
-  // },
-  // methods: {
-  //   add () {
-  //     this.text += ' add'
-  //   }
-  // }
+
   setup () {
-    const text = ref('abc')
-    const addedText = computed(() => text.value + ' added')
-    const add = () => {
-      text.value += ' add'
-    }
-
-    provide('text', text)
-
-    return {
-      text,
-      addedText,
-      add
-    }
+    //
   }
 })
 </script>
